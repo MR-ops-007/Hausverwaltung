a4d27c91cd24582725fb1ab308de0cae191395aa
MR-ops-007 <michael.raudies@googlemail.com>
2026-03-01 09:51:37 +0100

    Refactor data mapping for case insensitivity

--- data-service.js
+++ data-service.js
@@ -11,30 +11,34 @@ const dataService = {
     },
 
     setInitialData(data) {
-        console.log("DataService empfängt:", data);
-        // Robustes Mapping: Wir prüfen verschiedene Key-Schreibweisen
-        this.state.einheiten = data.einheiten || data.Einheiten || [];
-        this.state.mieter = data.mieter || data.Mieter || [];
-        this.state.zaehler_staende = data.zaehler_staende || data.Zaehler_Staende || data.zaehler || [];
-        this.state.parameter = data.parameter || data.Parameter || [];
+        console.log("DataService: Rohdaten erhalten", data);
         
-        console.log("State nach Mapping:", this.state);
+        // Google Sheets liefert oft ein Objekt, das die Tabellennamen als Keys hat
+        // Wir mappen hier extrem tolerant gegen Groß-/Kleinschreibung
+        const getSheet = (name) => {
+            return data[name] || data[name.toLowerCase()] || data[name.charAt(0).toUpperCase() + name.slice(1)] || [];
+        };
+
+        this.state.einheiten = getSheet('Einheiten');
+        this.state.mieter = getSheet('Mieter');
+        this.state.zaehler_staende = getSheet('Zaehler_Staende') || getSheet('Zaehler');
+        this.state.parameter = getSheet('Parameter');
+
+        console.log("DataService: State befüllt", this.state);
     },
 
     getUniqueObjects() {
-        // Falls einheiten leer ist, können wir nichts rendern
         if (!this.state.einheiten || this.state.einheiten.length === 0) return [];
-        
-        // Wir holen die Objektnamen und filtern Duplikate
-        const objects = this.state.einheiten.map(u => u.objekt || u.Objekt);
+        // Wir suchen nach 'Objekt' oder 'objekt' in den Spalten
+        const objects = this.state.einheiten.map(u => u.Objekt || u.objekt || u.Haus);
         return [...new Set(objects)].filter(obj => obj);
     },
 
     getUnitsByObject(objName) {
-        return this.state.einheiten.filter(u => (u.objekt || u.Objekt) === objName);
+        return this.state.einheiten.filter(u => (u.Objekt || u.objekt || u.Haus) === objName);
     },
 
     getActiveMieter(einheitId) {
-        return this.state.mieter.find(m => String(m.einheit_id || m.Einheit_ID) === String(einheitId));
+        return this.state.mieter.find(m => String(m.Einheit_ID || m.einheit_id) === String(einheitId));
     }
 };
