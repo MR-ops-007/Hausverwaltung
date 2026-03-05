6d906215459cd3297949b85dcda794c3a809da2e
MR-ops-007 <michael.raudies@googlemail.com>
2026-03-05 22:45:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,23 @@ const CONFIG = {
         units: 15,
         gewerbe: 1,
         meterTypes: ["KW", "WW", "Strom"], // WW ist hier für Mieter dabei!
+        defaultMeters: ["kaltwasser_m3", "warmwasser_m3", "strom_ht_kwh"],
+        customMeters: {
+            "LOK_Allgemein": ["kaltwasser_m3", "warmwasser_m3", "strom_ht_kwh", "strom_nt_kwh", "oel_stand_l"],
+        },
         hasOil: true
     },
-    hauptstrasse: {
+    "Ra-HS-29": {
         name: "🏢 Hauptstraße 29",
         units: 11,
         gewerbe: 2,
         excludeUnits: [9], // Diese Nummern werden übersprungen
         meterTypes: ["KW", "WW", "Strom"],
         generalMeters: ["Strom_HT", "Strom_NT", "KW_Haus"],
+        defaultMeters: ["kaltwasser_m3", "warmwasser_m3", "strom_ht_kwh"],
+        customMeters: {
+            "Ra-HS-29_Allgemein": ["kaltwasser_m3", "warmwasser_m3", "strom_ht_kwh", "strom_nt_kwh", "oel_stand_l"],
+        },
         hasOil: true
     }
 };
