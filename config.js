1337f24c4d130cc161b79f92a9c5b916fd790ccb
MR-ops-007 <michael.raudies@googlemail.com>
2026-06-18 15:18:54 +0200

    Update scriptUrl in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Die zentrale URL (Single Source of Truth)
-    scriptUrl: "https://script.google.com/macros/s/AKfycbzxoHx73MEgoiozjObY19mHqBGB8EjXstg5XtO_cOr6IorHZASC7cGH_YK8nLtGzvG74g/exec", 
+    scriptUrl: "https://script.google.com/macros/s/AKfycbzTZNQ_v25JvAXLRgWdZoqEoUZGe_OmZH_zh0dbheQXCHeIabnZ2ezOmbYGj6tiUBpZoA/exec", 
 
     // Die Objekt-Einstellungen
     "LOK": {
